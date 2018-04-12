43e0e94a398d3e7a96611210fec9ecf4a7f02179
claireduell <claireduell@192.168.1.81>
2018-04-12 21:49:09 +1200

    added timetable dao class

---
+++ TimeTableMerger/src/dao/TimetableDAO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import domain.Timetable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author claireduell
+ */
+public class TimetableDAO {
+    
+    private String URL = "jdbc:h2:tcp://localhost/~/test;IFEXISTS=TRUE";
+	
+	public TimetableDAO(){
+	}
+	
+	public TimetableDAO(String URL) {
+		this.URL = URL;
+	}
+    
+        		public void saveTimetable(Timetable timetable) {
+		String sql = "merge into Timetable (UserName, Password, FirstName, LastName, Email) values (?,?,?,?,?)";
+
+		try (
+				  Connection dbCon = JdbcConnection.getConnection(URL);
+				  PreparedStatement stmt = dbCon.prepareStatement(sql);) {
+
+			stmt.setString(1, user.getUserName());
+			stmt.setString(2, user.getPassword());
+			stmt.setString(3, user.getFirstName());
+			stmt.setString(4, user.getLastName());
+			stmt.setString(5, user.getEmail());
+
+			stmt.executeUpdate();
+
+		} catch (SQLException ex) {
+			throw new DAOException(ex.getMessage(), ex);
+		}
+	}
+}
