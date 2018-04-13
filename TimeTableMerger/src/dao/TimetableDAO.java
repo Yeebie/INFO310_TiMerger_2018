3a1eb41df474a2b51bab700d15611b83ffa5fa3f
Celine <celine.ahkit@gmail.com>
2018-04-13 16:28:08 +1200

    Editing the DAO classes

--- TimeTableMerger/src/dao/TimetableDAO.java
+++ TimeTableMerger/src/dao/TimetableDAO.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+
 /**
  *
  * @author claireduell
@@ -24,6 +25,124 @@ public class TimetableDAO {
 	public TimetableDAO(String URL) {
 		this.URL = URL;
 	}
+        
+        
+	public void getTimetable(Timetable timetable) {
+        }
+        /*
+        
+        //Example from 202
+	public Collection<Product> getProduct() {
+		String sql = "select * from Product";
+
+    try (
+        // get a connection to the database
+        Connection dbCon = JdbcConnection.getConnection(url);
+        // create the statement
+        PreparedStatement stmt = dbCon.prepareStatement(sql);
+    ) {
+        // execute the query
+        ResultSet rs = stmt.executeQuery();
+        // Using a List to preserve the order in which the data was returned from the query.
+        List<Product> products = new ArrayList<>();
+
+        // iterate through the query results
+        while (rs.next()) {
+
+            // get the data out of the query
+            Integer id = rs.getInt("product_id");
+            String name = rs.getString("name");
+				String description = rs.getString("description");
+				String category = rs.getString("category");
+            BigDecimal price = rs.getBigDecimal("price");
+				Integer quantity = rs.getInt("quantity_in_stock");
+
+            // use the data to create a product object
+				Product p = new Product(id, name, description, category, price, quantity);
+        
+            // and put it in the collection
+            products.add(p);
+		  }
+        return products;
+    } catch (SQLException ex) {
+        //throw new RuntimeException(ex);
+		  throw new DAOException(ex.getMessage(), ex);
+    }
+	}
+        
+        
+   */
+        
+        public void addTimeTable(Timetable timetable){
+            
+        }
+        /*
+       @Override
+	public void addProduct(Product product) {
+		String sql="merge into product (Product_ID, name, description, category, price, quantity_in_stock) values (?,?,?,?,?,?)";
+
+    try (
+        // get connection to database
+        Connection dbCon = JdbcConnection.getConnection(url);
+
+        // create the statement
+        PreparedStatement stmt = dbCon.prepareStatement(sql);
+    ) {
+        // copy the data from the product domain object into the SQL parameters
+        stmt.setInt(1, product.getProductID());
+        stmt.setString(2, product.getName());
+		  stmt.setString(3, product.getDescription());
+		  stmt.setString(4, product.getCategory());
+		  stmt.setBigDecimal(5, product.getPrice());
+		  stmt.setInt(6, product.getQuantityInStock());
+
+        stmt.executeUpdate();  // execute the statement
+
+    } catch (SQLException ex) {  // we are forced to catch SQLException
+        // don't let the SQLException leak from our DAO encapsulation
+        //throw new RuntimeException(ex);
+		  throw new DAOException(ex.getMessage(), ex);
+    }
+	}
+        
+        */
+        
+        
+        public void editTimeTable(Timetable timetable){
+            //this goes into the Timetable editor.java gui class
+        }
+        
+        
+        public void deleteTimeTable(Timetable timetable){
+            String sql = "delete from Timetable where UserName = ?";
+        }
+        
+        	/*@Override
+	public void deleteProduct(Product product) {
+		String sql = "delete from Product where Product_ID = ?";
+
+	try (
+        // get a connection to the database
+        Connection dbCon = JdbcConnection.getConnection(url);
+        // create the statement
+        PreparedStatement stmt = dbCon.prepareStatement(sql);){
+			  
+		  stmt.setString(1, product.getProductID().toString());
+		  stmt.executeUpdate();
+			 
+			  
+		} catch (SQLException ex) {
+        //throw new RuntimeException(ex);
+		  throw new DAOException(ex.getMessage(), ex);
+    }
+	}*/
+        
+        
+        
+        
+        
+        
+        
     /*
         		public void saveTimetable(Timetable timetable) {
 		String sql = "merge into Timetable (UserName, Password, FirstName, LastName, Email) values (?,?,?,?,?)";
--- TimeTableMerger/src/dao/UserDAO.java
+++ TimeTableMerger/src/dao/UserDAO.java
@@ -3,6 +3,7 @@ package dao;
 import domain.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -40,7 +41,10 @@ public class UserDAO {
 		}
 	}
 		
-			//public User findByUserName(String UserName) {
+                public void searchByUserName(String UserName){
+                    
+                }
+            //public User findByUserName(String UserName) {
 
 		//String sql = "select * from User where UserName = ?";
 
@@ -67,4 +71,139 @@ public class UserDAO {
 			//throw new DAOException(ex.getMessage(), ex);
 		//}
 	//}
+
+
+
+/*
+///
+	String url = "jdbc:h2:tcp://localhost:9018/project;IFEXISTS=TRUE;";
+	
+
+	public ManageCustomersDAO() {
+	}
+	
+	public ManageCustomersDAO(String url) {
+		this.url = url;
+	}
+	
+	public String getConnection(String url){
+		this.url = url;
+		return url; 
+	}
+	
+	@Override
+	public void save(Customer customer) {
+		String sql="merge into customer (username, name, emailAddress, shippingAddress, creditCardDetails, password) values (?,?,?,?,?,?)";
+
+    try (
+        // get connection to database
+        Connection dbCon = JdbcConnection.getConnection(url);
+
+        // create the statement
+        PreparedStatement stmt = dbCon.prepareStatement(sql);
+    ) {
+        // copy the data from the product domain object into the SQL parameters
+        stmt.setString(1, customer.getUsername());
+        stmt.setString(2, customer.getName());
+		  stmt.setString(3, customer.getEmailAddress());
+		  stmt.setString(4, customer.getAddress());
+		  stmt.setString(5, customer.getCreditCard());
+		  stmt.setString(6, customer.getPassword());
+
+        stmt.executeUpdate();  // execute the statement
+
+    } catch (SQLException ex) {  // we are forced to catch SQLException
+        // don't let the SQLException leak from our DAO encapsulation
+        //throw new RuntimeException(ex);
+		  throw new DAOException(ex.getMessage(), ex);
+    }
+	}
+	
+	@Override
+	public Customer getCustomer(String userName) {
+		String sql = "select * from Customer where username = ?";
+
+    try (
+        // get a connection to the database
+        Connection dbCon = JdbcConnection.getConnection(url);
+        // create the statement
+        PreparedStatement stmt = dbCon.prepareStatement(sql);
+    ) {
+        // execute the query
+		  stmt.setString(1, userName);
+        ResultSet rs = stmt.executeQuery();
+
+        // iterate through the query results
+        if(rs.next()) {
+            // get the data out of the query
+            String username = rs.getString("username");
+            String name = rs.getString("name");
+				String emailAddress = rs.getString("emailAddress");
+				String shippingAddress = rs.getString("shippingAddress");
+            String creditCardDetails = rs.getString("creditCardDetails");
+				String password = rs.getString("password");
+
+            // use the data to create a customer object
+				Customer c = new Customer(username, name, emailAddress, shippingAddress, creditCardDetails, password);
+				return c;
+        }else{
+			  return null;
+		  }
+   
+    } catch (SQLException ex) {
+        //throw new RuntimeException(ex);
+		  throw new DAOException(ex.getMessage(), ex);
+    }
+	}
+	/*
+		public Boolean validateCredentials(String userName, String password) {
+		if (customers.containsKey(userName)) {
+			return customers.get(userName).getPassword().equals(password);
+		} else {
+			return false;
+		}
+	}
+	
+*/
+
+//login 
+public Boolean validateCredentials(String userName, String passWord){
+return true; 
 }
+}
+
+/*
+	String sql = "select username, password from Customer";
+
+    try (
+        // get a connection to the database
+        Connection dbCon = JdbcConnection.getConnection(url);
+        // create the statement
+        PreparedStatement stmt = dbCon.prepareStatement(sql);
+    ) {
+        // execute the query
+		  stmt.setString(1, userName);
+		  stmt.setString(6, passWord);
+        ResultSet rs = stmt.executeQuery();
+
+        // iterate through the query results
+        if(rs.next()) {
+            // get the data out of the query
+            String username = rs.getString("username");
+				String password = rs.getString("password");
+
+				if(username.equals(userName) && password.equals(passWord)){
+					return true;
+				}else{
+					return false;
+				}
+			}
+   
+    } catch (SQLException ex) {
+        //throw new RuntimeException(ex);
+		  throw new DAOException(ex.getMessage(), ex);
+    }
+		return null;
+	}
+}
+*/
