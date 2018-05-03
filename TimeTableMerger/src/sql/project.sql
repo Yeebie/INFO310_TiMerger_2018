e7d722fbe8fc64e69805c1d3b2556f815d482757
Yeebo <yeeja359@student.otago.ac.nz>
2018-05-03 16:47:55 +1200

    Added code for calculateMeetingTimes, will need to be cleaned and moved to appropriate location. domain > MergedDay.java           | Added dupe class, has extra timeDuration datafields. gui > EditTimetable.java          | Added MergedDay support (Can we clean this up?). gui > ScheduleTimetable.java      | Added code to calculate length of gaps between busy periods. sql > project.sql                 | Added unused MergeDay table. References #31 Resolves #31 Notes: ScheduleTimetable's methods need to be removed. MergedDay needs to extend Day, needs to be revisited. project's MergeDay table was created when I had no clue what I was doing, may end up keeping it.

--- TimeTableMerger/src/sql/project.sql
+++ TimeTableMerger/src/sql/project.sql
@@ -47,6 +47,44 @@ create table Day (
 );
 
 /**
+Do we want to save the MergedDay in the database? Is it necessary?
+
+create table mergedDay (
+	GroupName varchar(255) NOT NULL, -- Can we get a combination of userIDs? Will this be a custom groupname?
+	DayName varchar(20) NOT NULL,
+	eightAM boolean NOT NULL,
+        eightAMDuration Integer NOT NULL,
+	nineAM boolean NOT NULL,
+        nineAMDuration Integer NOT NULL,
+	tenAM boolean NOT NULL,
+        tenAMDuration Integer NOT NULL,
+	elevenAM boolean NOT NULL,
+        elevenAMDuration Integer NOT NULL,
+	twelvePM boolean NOT NULL,
+        twelvePMDuration Integer NOT NULL,
+	onePM boolean NOT NULL,
+        onePMDuration Integer NOT NULL,
+	twoPM boolean NOT NULL,
+        twoPMDuration Integer NOT NULL,
+	threePM boolean NOT NULL,
+        threePMDuration Integer NOT NULL,
+	fourPM boolean NOT NULL,
+        fourPMDuration Integer NOT NULL,
+	fivePM boolean NOT NULL,
+        fivePMDuration Integer NOT NULL,
+	sixPM boolean NOT NULL,
+        sixPMDuration Integer NOT NULL,
+	sevenPM boolean NOT NULL,
+        sevenPMDuration Integer NOT NULL,
+	eightPM boolean NOT NULL,
+        eightPMDuration Integer NOT NULL,
+	ninePM boolean NOT NULL,
+        ninePMDuration Integer NOT NULL,
+	primary key (UserName, DayName),
+	foreign key (UserName) references User(UserName),
+);*/
+
+/**
  * Notes:
  * False represents a free time slot
  * True represents a busy time slot
