71e2b28087d3c048793559370b6f94c7c96549e1
Sony <Sony@Sony-PC>
2018-05-19 14:32:56 +0300

    added a testing skeleton but still not passed yet.

--- TimeTableMerger/test/userGUI/GuiCreateAccountTesting.java
+++ TimeTableMerger/test/userGUI/GuiCreateAccountTesting.java
@@ -3,8 +3,10 @@ package userGUI;
 import dao.TimetableDAO;
 import dao.UserDAO;
 import dao.UserStorageDAO;
+import domain.Day;
 import domain.User;
 import gui.CreateAccount;
+import gui.CreateTimetable;
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.TreeSet;
@@ -28,64 +30,89 @@ import org.mockito.stubbing.Answer;
  */
 public class GuiCreateAccountTesting {
 
-	private UserDAO userDao;
+	private UserDAO userDao = new UserDAO ("jdbc:h2:tcp://localhost:9054/project-testing");
+        private User user;
+        private CreateAccount createAccount;
 	private UserStorageDAO userStorageDao;
 	private TimetableDAO timetableDao;
 	private DialogFixture fixture;
 	private Robot robot;
 	private User user1;
 	private User user2;
-	private Collection<User> users = new TreeSet<>();
+	
 
 	public GuiCreateAccountTesting() {
 	}
 
 	@Before
 	public void setUp() {
-		robot = BasicRobot.robotWithNewAwtHierarchy();
-		robot.settings().delayBetweenEvents(75);
 
-		this.user1 = new User("username1", "firstname1", "lastname1", "password1", "email@email.com");
-		this.user2 = new User("username2", "firstname2", "lastname2", "password2", "email@email.com");
-		users.add(user1);
-		users.add(user2);
+	robot = BasicRobot.robotWithNewAwtHierarchy();
+        robot.settings().delayBetweenEvents(75);
 
-		userDao = mock(UserDAO.class);
-		userStorageDao = mock(UserStorageDAO.class);
-		timetableDao = mock(TimetableDAO.class);
-	}
+        this.user1 = new User("test", "user1", "true", "true", "true");
+        this.user2 = new User("test", "user2", "true", "true" , "true");
 
+        timetableDao = mock(TimetableDAO.class);
+        userDao = mock(UserDAO.class);
+        userStorageDao = mock(UserStorageDAO.class);
+    }
 	@After
 	public void tearDown() {
+            System.out.println("huhuhu");
 		fixture.cleanUp();
+                
+                
+
 	}
-	
-		@Test
-	public void testSaveUser() {
-		CreateAccount create = new CreateAccount(userDao, timetableDao, userStorageDao);
-		fixture = new DialogFixture(robot, create);
+
+	// TODO add test methods here.
+	// The methods must be annotated with annotation @Test. For example:
+	//
+	// @Test
+	// public void hello() {}
+	@Test
+	public void testSave() {
+            
+           
+             //CreateTimetable create = new CreateTimetable(null, true, timetableDao, userDao, userStorageDao);
+                CreateAccount create = new CreateAccount (userDao ,timetableDao ,userStorageDao);
+		// create the dialog passing in the mocked DAO
+		
+
+		// use AssertJ to control the dialog
+		fixture = new DialogFixture(robot , create );
 		fixture.show().requireVisible();
 
-		fixture.textBox("userNameField").enterText("username3");
-		fixture.textBox("firstNameField").enterText("firstName3");
-		fixture.textBox("lastNameField").enterText("lastName3");
-		fixture.textBox("passwordField").enterText("password3");
-		fixture.textBox("reenterField").enterText("password3");
-		fixture.textBox("emailField").enterText("email@email.com");
+		// enter some details into the UI components
+		fixture.textBox("usernameField").enterText("Alzhu850");
+		fixture.textBox("firstNameField").enterText("Hussam");
+		fixture.textBox("lastNameField").enterText("Alzahrani");
+		fixture.textBox("emailField").enterText("h22m@hot.com");
+		fixture.textBox("passwordField").enterText("12345");
+		
 
+		// click the save button
 		fixture.button("loginButton").click();
 
+		// create a Mockito argument captor to use to retrieve the passed student from the mocked DAO
 		ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
 
-		verify(userDao).saveUser(argument.capture());
+		 System.out.println("testing" + "argument");
+       
+        //      System.out.println(argument.capture() + "capture" ) ; 
+        //    System.out.println(argument + "argument") ; 
+                 verify(userDao).saveUser(argument.capture());
 
-		User savedUser = argument.getValue();
+                 User saveUser = argument.getValue();
 
-		assertEquals("Ensure the username was saved", "username3", savedUser.getUserName());
-		assertEquals("Ensure the first name was saved", "firstName3", savedUser.getFirstName());
-		assertEquals("ensure the last name was saved", "lastName3", savedUser.getLastName());
-		assertEquals("Ensure the password was saved", "password3", savedUser.getPassword());
-		assertEquals("Ensure the email was saved", "email@email.com", savedUser.getEmail());
+		// test that the product details were properly saved
+		assertEquals("Ensure the userName was saved", "Alzhu850",  saveUser.getUserName());
+		assertEquals("Ensure the firstName was saved", "Hussam", saveUser.getFirstName());
+		assertEquals("Ensure the lastName was saved", "Alzahrani", saveUser.getLastName());
+		assertEquals("Ensure the email was saved", "h22m@hot.com" , saveUser.getEmail());
+                assertEquals("Ensure the password was saved", "12345", saveUser.getPassword());
+		
+
 	}
-
 }
